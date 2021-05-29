8639bd9831787f0f04b9863b22f2813cfcb21e6e
joohoonkim <davidkim2552@gmail.com>
2021-05-29 14:56:42 -0400

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
+import * as d3 from 'd3'
+
+const csvUrl = 'https://gist.githubusercontent.com/kim2552/b92cb98de8ad456cc4fcbd644af568ce/raw/cssNamedColors'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [data,setData] = useState(null)
+
+  useEffect(() => {
+    d3.csv(csvUrl).then(setData)
+  }, [])
+
+  if(!data){
+
+    return(
+    <pre>Loading...</pre>
+    )
+
+  }else{
+    console.log(data[0])
+    
+    return(
+      data.map(d =>
+        <div style={{
+          backgroundColor: d['RGB hex value'],
+          width: "100%",
+          height: "0.5rem"
+        }}></div>
+        )
+    )
+
+  }
 }
 
 export default App;
