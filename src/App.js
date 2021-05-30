a87bc54501c67e7d172d1add1a9229f9900e86be
joohoonkim <davidkim2552@gmail.com>
2021-05-30 11:23:29 -0400

    added timeline

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 
 import {useData}  from './useData'
 import {Marks} from './Marks'
+import Timeline from './Timeline'
 
 const topoJsonUrl = 'https://gist.githubusercontent.com/Brideau/2391df60938462571ca9/raw/f5a1f3b47ff671eaf2fb7e7b798bacfc6962606a/canadaprovtopo.json'
 
 function App() {
   const data = useData(topoJsonUrl)
+
+  function handleSelection(event){
+
+    // Reset all other points except for selected point
+    const points = document.getElementsByClassName('point')
+    for(let p of points){
+      if(p.id !== event.target.id){
+        document.getElementById(p.id).setAttribute("r","5")
+      }
+    }
+    
+    // Handles selection of either parent or children elements
+    if(event.target.childNodes !== 0){
+      event.target.childNodes.forEach((e) => {
+        document.getElementById(e.id).setAttribute("r","10")})
+    }else{
+      document.getElementById(event.target.id).setAttribute("r","10")
+    }
+  }
 
   if(!data){
 
@@ -22,6 +42,7 @@ function App() {
           data={data}
           ></Marks>
         </div>
+        <Timeline handleSelection={handleSelection}></Timeline>
         <h1>Hello World</h1>
       </div>
     )
---
+++ src/Timeline.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+
+export class TimeLine extends Component {
+    constructor(props){
+        super(props);
+
+        const currentYear  = new Date().getFullYear();
+        this.time_period_list = new Array(currentYear-2000);
+        for(var i=0; i<this.time_period_list.length; i++){
+            this.time_period_list[i] = 2001+i;
+        }
+    }
+
+    getTimelineElement(){
+        const time_periods = []
+        for(const[index, date] of this.time_period_list.entries()){
+            time_periods.push(
+                <div className="swiper-wrapper" key={index}>
+                    <div className="point-wrapper">
+                        <svg height="30" width="30" onMouseOver={(e)=>{this.props.handleSelection(e)}}>
+                            <circle className="point" id={index} cx="15" cy="15" r="5" stroke="black" strokeWidth="1" fill="red"/>
+                        </svg>
+                    </div>
+                    <p>{date}</p>
+                </div>
+            )
+        }
+        return time_periods;
+    }
+
+    mouseScroll = (e) => {
+        var container = document.getElementById('timeline-carousel')
+        var containerScrollPosition = document.getElementById('timeline-carousel').scrollLeft
+        container.scrollTo({
+            top: 0,
+            left: containerScrollPosition + e.deltaY,
+            behaviour: 'smooth' //if you want smooth scrolling
+        })
+    }
+
+    render() {
+        const time_periods = this.getTimelineElement();
+        return (
+        <div className="timeline-container">
+            <div className="carousel" id="timeline-carousel" onWheel={(e) =>{this.mouseScroll(e)}}>
+                {time_periods}
+            </div>
+        </div>
+        )
+    }
+}
+
+export default TimeLine
