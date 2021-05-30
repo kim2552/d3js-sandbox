6966ab8c7f06a588c73561b9c95122948a7984de
joohoonkim <davidkim2552@gmail.com>
2021-05-30 00:02:54 -0400

    proper styled map. interactive

--- src/Marks.js
+++ src/Marks.js
@@ -7,17 +7,36 @@ export const Marks = ({ data:{provinces, interiors }}) => {
     const svgHeight=720
     
     const projection = geoTransverseMercator()
-    .scale(600)
-    .translate([svgWidth/2, svgHeight/2+150])
+    .scale(800)
+    .translate([svgWidth/2-100, svgHeight/2+280])
     .rotate([96, 0])
     .center([-0.6, 38.7]);
   
     const path = geoPath(projection)
 
+    const mouseClick = (event,feature) => {
+        const provinces = document.getElementsByClassName('provinces')
+        for(let p of provinces){
+            p.classList.remove("interactive")
+        }
+        event.target.classList.add("interactive")
+    }
+
+    const mouseHover = (event,feature) => {
+        const provinces = document.getElementsByClassName('provinces')
+        for(let p of provinces){
+            p.classList.remove("interactive")
+        }
+        event.target.classList.add("interactive")
+    }
+
     return (
-        <svg 
-        width={svgWidth} 
-        height={svgHeight*3/4}
+        <svg
+        id="svg"
+        width="100%"
+        preserveAspectRatio="xMinYMin meet"
+        viewBox="0 0 730 650"
+        style={{border:"1px", borderStyle: "solid", borderColor: "black"}}
         >
             <g className="marks">
                 {
@@ -26,6 +45,8 @@ export const Marks = ({ data:{provinces, interiors }}) => {
                         className="provinces"
                         key={i}
                         d={path(feature)}
+                        onClick={(event)=>mouseClick(event,feature)}
+                        onMouseOver={(event)=>mouseHover(event,feature)}
                         ></path>
                     ))
                 }
